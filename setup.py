c5c5e4588ffad89aff94de3b460b069a13a0177a
EliousMondal <mdeliousali@gmail.com>
2020-08-03 08:05:06 +0530

    1D_TISE.pyx created ,1D_TISE_c.pyx removed, 1D_TISE.py renamed to 1D_TISE_py.py, setup.py modified to 1D_TISE.pyx as the file to cythonize

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize("1D_TISE_c.pyx",annotate=True))
+setup(ext_modules=cythonize("1D_TISE.pyx",annotate=True))
